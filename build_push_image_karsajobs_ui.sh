d45da4e9b2c184175e65b9eff92b3278112ae647
Muhammad Franklin Yusuf <yusuffranklin@gmail.com>
2023-08-23 15:10:02 +0700

    Update build_push_image_karsajobs_ui.sh

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -4,7 +4,7 @@
 sudo docker build -t yusuffranklin/karsajobs-ui:latest .
 
 # login ke Docker Hub via terminal
-sudo docker login
+sudo docker login -u yusuffranklin --password-stdin $PASSWORD
 
 # push docker container ke repository Docker Hub
 sudo docker push yusuffranklin/karsajobs-ui:latest
